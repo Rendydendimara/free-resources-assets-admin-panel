32ec5754969a211821d3a63a8f53856dd90e469c
“Rendy <r3ndydinar@gmail.com>
2024-05-28 23:10:24 +0800

    feat/create-module:     - User     - Category     - Collection     - Product     - Membership     - Package

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -5,8 +5,14 @@ import { mongooseAdapter } from '@payloadcms/db-mongodb'
 import { webpackBundler } from '@payloadcms/bundler-webpack'
 import { slateEditor } from '@payloadcms/richtext-slate'
 import { buildConfig } from 'payload/config'
-
-import Users from './collections/Users'
+import Users from './collections/Users/Users'
+import Media from './collections/Media'
+import Customers from './collections/Customer/Customer'
+import Category from './collections/Category/Category'
+import Package from './collections/Package/Package'
+import Collection from './collections/Collection/Collection'
+import Product from './collections/Product/Product'
+import Membership from './collections/Membership/Membership'
 
 export default buildConfig({
   admin: {
@@ -14,7 +20,7 @@ export default buildConfig({
     bundler: webpackBundler(),
   },
   editor: slateEditor({}),
-  collections: [Users],
+  collections: [Users, Media, Customers, Category, Package, Collection, Product, Membership],
   typescript: {
     outputFile: path.resolve(__dirname, 'payload-types.ts'),
   },
@@ -25,4 +31,21 @@ export default buildConfig({
   db: mongooseAdapter({
     url: process.env.DATABASE_URI,
   }),
+  express: {
+    preMiddleware: [
+      (req, res, next) => {
+        // do something here
+        console.log('middleware')
+        next()
+      }
+    ],
+  },
+  rateLimit: {
+    trustProxy: true,
+  },
+  csrf: [
+    // whitelist of domains to allow cookie auth from
+    'http://localhost:3000',
+  ],
+  cors: '*' // allow any domain
 })
